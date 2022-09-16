fdbcfc4fb84e36d55fa1fbeba63993b4148a3b51
ahmed abdullah <Ahmed Abdullah>
2022-09-16 11:52:04 +0500

    version1.0

---
+++ elevator.js
@@ -0,0 +1,54 @@
+var id = null;
+var idD = null;
+var pos = 0;
+var prfm = 0;
+let elem = document.getElementById("liftDiv");
+let fDoor = document.getElementById("firstD");
+let sDoor = document.getElementById("secondD");
+let btn = document.getElementsByClassName("btns");
+
+
+//Door Animation
+function liftDoors(c) {
+  clearInterval(idD);
+  idD = setInterval(frame1, 5);
+  function frame1() {
+    if (prfm == c) {
+      clearInterval(idD);
+    }
+    else {
+      if (c == 0) { prfm-- }
+      else { prfm++ }
+      fDoor.style.width = prfm + 'px';
+      sDoor.style.width = prfm + 'px';
+    }
+  }
+}
+
+//Lift Animation
+function liftMovement(a) {
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].disabled = true;
+    btn[i].style.opacity = '0.4'
+  }
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  liftDoors(49);
+  function frame() {
+    if (pos == a) {
+      clearInterval(id);
+      liftDoors(0);
+      for (let i = 0; i < btn.length; i++) {
+        btn[i].disabled = false;
+        btn[i].style.opacity = '1';
+      }
+    }
+    else {
+      if (a == 0) { pos-- }
+      else if (pos > a) { pos-- }
+      else { pos++ }
+      elem.style.bottom = pos + 'px';
+      
+    }
+  }
+}
